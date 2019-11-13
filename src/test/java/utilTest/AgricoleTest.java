e156fc3f7378e1608ec3c62ec9198960ff84e6af
GitHubA18 <56780224+GitHubA18@users.noreply.github.com>
2019-11-13 16:47:21 -0500

    modification des valeurs de superficie des methodes de test

--- src/test/java/utilTest/AgricoleTest.java
+++ src/test/java/utilTest/AgricoleTest.java
@@ -13,6 +13,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import util.Utilitaire;
 
 /**
  *
@@ -56,42 +57,74 @@ public class AgricoleTest {
   
     @Test
     public void testCalculerValeurFonciereLot_m2_v1() {
-        double superficie=4.50;
-        double prix_min= 5;
+        double superficie=150.00;
+        double prix_min= 45.5;
+        double resultatAttendu = 6825;
         
-        Terrain t1 = new Terrain(0, 5, 8);
-        double resultat = superficie * (t1.getPrix_m2_min());
-        double resultatAttendu = superficie * prix_min;
-                resultatAttendu =22.5;
+        Terrain t1 = new Terrain(0, 45.5, 8);
+        double retour = superficie * (t1.getPrix_m2_min());
+        double resultat = (Utilitaire.arrondirDecimales(retour, 2));
+        
         assertTrue("résultat attendu: " + resultatAttendu + ", mais résultat obtenu: " + resultat, resultatAttendu == resultat);
     }
 
         @Test
     public void testCalculerValeurFonciereLot_m2_v2() {
-        double superficie=90.5;
-        double prix_min= 105.50;
+        double superficie=100.00;
+        double prix_min= 105.55;
+          double resultatAttendu = 10555;
+          
+        Terrain t2 = new Terrain(0, 105.55, 6);
+        double retour = superficie * (t2.getPrix_m2_min());
+        double resultat = (Utilitaire.arrondirDecimales(retour, 2));
+              
+        assertTrue("résultat attendu: " + resultatAttendu + ", mais résultat obtenu: " + resultat, resultatAttendu == resultat);
+    }
+    
+            @Test
+    public void testCalculerValeurFonciereLot_m2_v3() {
+        double superficie=120.00;
+        double prix_min= 105.457;
         
-        Terrain t2 = new Terrain(0, 105.50, 8);
-        double resultat = superficie * (t2.getPrix_m2_min());
+        Terrain t3 = new Terrain(0, 105.457, 9);
+        double retour = superficie * (t3.getPrix_m2_min());
         
     double resultatAttendu = superficie * prix_min;
-                resultatAttendu =9547.75;
+                resultatAttendu =12654.84;
+    double  resultat = (Utilitaire.arrondirDecimales(retour, 2));
         assertTrue("résultat attendu: " + resultatAttendu + ", mais résultat obtenu: " + resultat, resultatAttendu == resultat);
     }
     
     @Test
-    public void testcalculerDroitsLot() {
+    public void testcalculerDroitsLot_v1() {
         
                 int nbDroits = 4;
         double valeurLot = 100.25;
-        double resultatAttendu = 479.95;
+        double resultatAttendu = 500-(4*100.25*0.05);
+              //  479.95;
         
         Lot lot1 = new Lot("lot 1", 4, 0, 465, "2015-10-14");
         
-        double resultat = 500.00 - (lot1.getNombre_droits_passage() * valeurLot * 0.05);
-  
+        double retour = 500.00 - (lot1.getNombre_droits_passage() * valeurLot * 0.05);
+          double  resultat = (Utilitaire.arrondirDecimales(retour, 2));
+          
+        assertTrue("résultat attendu: " + resultatAttendu + ", mais résultat obtenu: " + resultat, resultatAttendu == resultat);
 
-        //double retour = MONTANT_BASE - (lot.getNombre_droits_passage() * valeurLot * COEF_DROITS);
+    }
+    
+        @Test
+    public void testcalculerDroitsLot_v2() {
+        
+        int nbDroits = 8;
+        double valeurLot = 100.257;
+        double resultatAttendu = 459.9;
+              //  479.95;
+        
+        Lot lot3 = new Lot("lot 3", 8, 3, 3333, "2013-06-20");
+        
+        double retour = 500.00 - (lot3.getNombre_droits_passage() * valeurLot * 0.05);
+        double resultat = (Utilitaire.arrondirDecimales(retour, 2));
+          
         assertTrue("résultat attendu: " + resultatAttendu + ", mais résultat obtenu: " + resultat, resultatAttendu == resultat);
 
     }
