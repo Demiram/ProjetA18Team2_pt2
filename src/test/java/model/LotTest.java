a27bfec12b661cab93e3c72f90b15cb8c84b999d
1995160 <1995160@LI-h016-0T24>
2019-12-05 17:18:36 -0500

    methodes de tests pour le modele

---
+++ src/test/java/model/LotTest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import modele.Lot;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author 1995160
+ */
+public class LotTest {
+    
+    public LotTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    
+    
+//     @Test
+//     public void test_formatterDateISO8601() {
+//         Lot lot = new Lot("boum", 1, 1, 1.0, "2000/10/25");
+//         String dateAttendue = "2000-10-25";
+//         assertTrue("date attendue: "+dateAttendue+"; date obtenue: "+lot.getDate_mesure(), dateAttendue.equals(lot.getDate_mesure()));
+//     }
+//     
+     
+}
