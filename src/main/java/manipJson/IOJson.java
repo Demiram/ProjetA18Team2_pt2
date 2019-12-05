214bc51cc1b9403966795ad4c483307a6751df62
0249248 <0249248@LI-H016-0T25>
2019-12-05 13:07:54 -0500

    styling et correction sur un test (list erreur)

--- src/main/java/manipJson/IOJson.java
+++ src/main/java/manipJson/IOJson.java
@@ -20,27 +20,28 @@ import net.sf.json.JSONObject;
  * @author Demi
  */
 public class IOJson {
+
     public static Terrain traiterEntreeTerrain(String inputPath) throws IOException {
         String json = FileReader.loadFileIntoString(inputPath);
         Terrain terrain = new Terrain();
         List<Lot> entreeLotsArr = new ArrayList<Lot>();
         Lot lotEntreeJson = new Lot();
-        
+
         String entreeDescriptionLot;
         int entreeNbDroitsPassageLot;
         int entreeNbServicesLot;
         int entreeSuperficieLot;
         String entreeDateMesureLot;
-        
+
         JSONObject mainObject = JSONObject.fromObject(json);
-        
+
         terrain.setType_terrain(mainObject.getInt("type_terrain"));
         terrain.setPrix_m2_min(validerEntreeDouble(mainObject.getString("prix_m2_min")));
         terrain.setPrix_m2_max(validerEntreeDouble(mainObject.getString("prix_m2_max")));
-        
+
         JSONArray lots = mainObject.getJSONArray("lotissements");
         JSONObject singleLot;
-        for(int i = 0;i < lots.size();i++) {
+        for (int i = 0; i < lots.size(); i++) {
             singleLot = lots.getJSONObject(i);
 
             entreeDescriptionLot = singleLot.getString("description");
@@ -48,75 +49,76 @@ public class IOJson {
             entreeNbServicesLot = singleLot.getInt("nombre_services");
             entreeSuperficieLot = singleLot.getInt("superficie");
             entreeDateMesureLot = singleLot.getString("date_mesure");
-            entreeLotsArr.add(new Lot(entreeDescriptionLot,entreeNbDroitsPassageLot,
-                entreeNbServicesLot,entreeSuperficieLot,entreeDateMesureLot));
-            
+            entreeLotsArr.add(new Lot(entreeDescriptionLot, entreeNbDroitsPassageLot,
+                    entreeNbServicesLot, entreeSuperficieLot, entreeDateMesureLot));
+
             entreeDescriptionLot = "";
             entreeNbDroitsPassageLot = 0;
             entreeNbServicesLot = 0;
             entreeSuperficieLot = 0;
             entreeDateMesureLot = "";
         }
-        
+
         terrain.setLots(entreeLotsArr);
-        
+
         return terrain;
     }
-    
+
     public static void traiterSortieTerrain(Terrain terrain, String outputPath) throws IOException {
         JSONObject mainObject = new JSONObject();
         List<Lot> lots = terrain.getLots();
         JSONArray lotsJson = new JSONArray();
         JSONObject singleLot = new JSONObject();
-        
+
         mainObject.accumulate("valeur_fonciere_totale", terrain.getValeur_fonciere_totale() + " $");
         mainObject.accumulate("taxe_scolaire", terrain.getTaxe_scolaire() + " $");
         mainObject.accumulate("taxe_municipale", terrain.getTaxe_municipale() + " $");
-        
-        for(int i = 0; i < lots.size(); i++) {
-            
+
+        for (int i = 0; i < lots.size(); i++) {
+
             singleLot.accumulate("description", lots.get(i).getDescriptionEntree());
             singleLot.accumulate("valeur_par_lot", lots.get(i).getValeur_par_lot() + " $");
             lotsJson.add(singleLot);
             singleLot.clear();
         }
-        
+
         mainObject.accumulate("lotissements", lotsJson);
         FileWriter.saveStringIntoFile(outputPath, mainObject.toString());
     }
-    
+
     public static JSONObject lancerErreur(String message) {
         JSONObject mainObject = new JSONObject();
-        
+
         mainObject.accumulate("message", message);
-        
+
         return mainObject;
     }
-    
+
     public static double validerEntreeDouble(String str) {
         StringBuilder strB = new StringBuilder();
-        
-        for(int i = 0; i<str.length();i++) {
-            if(str.charAt(i)== ',') {
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ',') {
                 strB.append('.');
-            } else if(str.charAt(i) != ' ' && str.charAt(i) != '$') {
+            } else if (str.charAt(i) != ' ' && str.charAt(i) != '$') {
                 strB.append(str.charAt(i));
             }
         }
-        
+
         return Double.parseDouble(strB.toString());
     }
+
     public static JSONObject lancerErreur(List<String> message) {
         JSONObject mainObject = new JSONObject();
         JSONArray messages = new JSONArray();
         JSONObject singleMessage = new JSONObject();
-        
-        for(int i = 0; i < message.size(); i++) {
+
+        for (int i = 0; i < message.size(); i++) {
             singleMessage.accumulate("message" + i, message.get(i));
             messages.add(singleMessage);
             singleMessage.clear();
         }
-        
+
         mainObject.accumulate("messages", messages);
         return mainObject;
     }
